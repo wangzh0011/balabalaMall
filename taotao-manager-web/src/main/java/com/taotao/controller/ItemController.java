2faf7c826c096627613f245a80dbd400b1d867c7
alan.wang <wang_zonghua@163.com>
2018-12-07 17:38:28 +0800

    add

---
+++ taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -0,0 +1,21 @@
+package com.taotao.controller;
+
+import com.taotao.pojo.TbItem;
+import com.taotao.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @RequestMapping("/item/{itemid}")
+    @ResponseBody
+    public TbItem getTbItem(@PathVariable long itemid){
+        TbItem tbItem = itemService.getItemById(itemid);
+        return tbItem;
+    }
+}
