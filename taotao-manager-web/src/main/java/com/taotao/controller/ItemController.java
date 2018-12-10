0b3e8386f8c2d68fa0536ccec754e093e175a2ac
alan.wang <wang_zonghua@163.com>
2018-12-10 15:52:46 +0800

    add

--- taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -3,10 +3,11 @@ package com.taotao.controller;
 import com.taotao.pojo.TbItem;
 import com.taotao.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+@Controller
 public class ItemController {
 
     @Autowired
--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ItemServiceImpl.java
@@ -1,10 +1,11 @@
-package com.taotao;
+package com.taotao.service;
 
 import com.taotao.mapper.TbItemMapper;
 import com.taotao.pojo.TbItem;
-import com.taotao.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ItemServiceImpl implements ItemService {
 
     @Autowired
